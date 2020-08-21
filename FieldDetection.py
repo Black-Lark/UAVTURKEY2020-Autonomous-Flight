26990465ca4a72adb4d7eb7239b6cde60c91effa
Bilal KABAŞ <bilalkabas@icloud.com>
2020-08-22 00:15:59 +0300

    Update FieldDetection.py

--- FieldDetection.py
+++ FieldDetection.py
@@ -10,51 +10,22 @@ while(cap.isOpened()):
 
     if ret == True:
         # Filter red color
+        # frame = cv2.bilateralFilter(frame,9,75,75)
         frame_hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-
         mask1 = cv2.inRange(frame_hsv, (0, 70, 50), (10, 255, 255))
         mask2 = cv2.inRange(frame_hsv, (170, 70, 50), (180, 255, 255))
         mask = mask1 + mask2
-
-        canny_output = cv2.Canny(mask, 10, 10 * 2)
-        # contours, _ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        # for i in range(len(contours)):
-        #     cv2.drawContours(img, contours, i, (255,255,255), 1)
-
-        # img = cv2.cvtColor(canny_output,cv2.COLOR_BGR2GRAY)
-
-        pixels = np.where(img == 255)
-        
+        # Edge detection
+        canny_output = cv2.Canny(mask, 10, 100, 100)
+        # Find the center
+        pixels = np.where(canny_output == 255)
         cX = np.average(pixels[1])
         cY = np.average(pixels[0])
         
         # cX cY -> NaN
-        
-        # img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-        cv2.circle(img, (int(cX),int(cY)), 3, (0,255,255), thickness=10, lineType=8, shift=0)
-
-        cv2.imshow("contours", img)
-
-        # # Get the moments
-        # mu = [None]*len(contours)
-        # for i in range(len(contours)):
-        #     mu[i] = cv2.moments(contours[i])
-        # # Get the mass centers
-        # mc = [None]*len(contours)
-        # for i in range(len(contours)):
-        #     # add 1e-5 to avoid division by zero
-        #     mc[i] = (mu[i]['m10'] / (mu[i]['m00'] + 1e-5), mu[i]['m01'] / (mu[i]['m00'] + 1e-5))
-        # # Draw contours
-        
-        # drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-        
-        # for i in range(len(contours)):
-        #     cv2.drawContours(drawing, contours, i, (0,255,255), 2)
-        #     # cv2.circle(drawing, (int(mc[i][0]), int(mc[i][1])), 4, (0,255,255), -1)
-        
-        # # cv2.imshow('Contours1', mask)
-        # cv2.imshow('Contours2', drawing)
+        cv2.circle(canny_output, (int(cX),int(cY)), 3, (255,255,255), thickness=10, lineType=8, shift=0)
+        # Show the frame
+        cv2.imshow("contours", canny_output)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Video stream has been terminated.')
