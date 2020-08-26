0a065bafa2cc6c65b8f56540c303bb9405b6af01
Bilal KABAŞ <bilalkabas@icloud.com>
2020-08-26 11:43:19 +0300

    Update client.py

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import picamera
 
 client_socket = socket.socket()
 
-client_socket.connect(('', 8000))  # ADD IP HERE
+client_socket.connect(('192.168.137.1', 8000))  # ADD IP HERE
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
