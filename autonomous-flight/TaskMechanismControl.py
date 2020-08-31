494183f02d0d4ab14d1ee20ae766b0006ca75f1c
Bilal KABAŞ <bilalkabas@icloud.com>
2020-08-31 20:42:31 +0300

    Update TaskMechanismControl.py

--- autonomous-flight/TaskMechanismControl.py
+++ autonomous-flight/TaskMechanismControl.py
@@ -1,4 +1,5 @@
 from gpiozero import LED, Button
+from signal import pause 
 import time
 
 level0 = Button(6)
@@ -7,9 +8,14 @@ level2 = Button(19)
 level3 = Button(26)
 
 
-while True:
-    print("Level 0: ", level0.is_pressed)
-    print("Level 1: ", level1.is_pressed)
-    print("Level 2: ", level2.is_pressed)
-    print("Level 3: ", level3.is_pressed)
-    time.sleep(1)
+# while True:
+#     print("Level 0: ", level0.is_pressed)
+#     print("Level 1: ", level1.is_pressed)
+#     print("Level 2: ", level2.is_pressed)
+#     print("Level 3: ", level3.is_pressed)
+#     time.sleep(1)
+
+
+dcmotor = LED(21)
+
+pause()
