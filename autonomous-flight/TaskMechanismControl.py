cec9eef6a40b9237c76a896cb4d721b808a8a672
Bilal KABAŞ <bilalkabas@icloud.com>
2020-08-31 20:05:41 +0300

    Update TaskMechanismControl.py

--- autonomous-flight/TaskMechanismControl.py
+++ autonomous-flight/TaskMechanismControl.py
@@ -8,8 +8,8 @@ level3 = Button(26)
 
 
 while True:
-    print("Level 0: " + level0)
-    print("Level 1: " + level1)
-    print("Level 2: " + level2)
-    print("Level 3: " + level3)
+    print("Level 0: ", level0)
+    print("Level 1: ", level1)
+    print("Level 2: ", level2)
+    print("Level 3: ", level3)
     time.sleep(1)
