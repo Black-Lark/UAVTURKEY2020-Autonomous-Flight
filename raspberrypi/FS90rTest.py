854ac7ad46a7e8e5ce1603abfdfe8ab61d99b03f
AbdulkadirCeylan <abdulkadir.ceylan@agu.edu.tr>
2020-09-01 12:43:44 +0300

    Update FS90rTest.py

--- raspberrypi/FS90rTest.py
+++ raspberrypi/FS90rTest.py
@@ -9,7 +9,7 @@ GPIO.setup(13, GPIO.OUT)
 pwm=GPIO.PWM(13, 50)
 pwm.start(0)
 
-pwm.ChangeDutyCycle(5) # left -90 deg position
+pwm.ChangeDutyCycle(10) # left -90 deg position
 time.sleep(2)
 pwm.stop()
 GPIO.cleanup()
