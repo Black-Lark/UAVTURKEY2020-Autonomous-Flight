703d3b22e753eb39c7674462467861b32ffde363
AbdulkadirCeylan <abdulkadir.ceylan@agu.edu.tr>
2020-09-01 12:32:30 +0300

    FS90rTest Create

---
+++ raspberrypi/FS90rTest.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.OUT)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.OUT)
+
+pwm.ChangeDutyCycle(5) # left -90 deg position
+
+pwm.stop()
+GPIO.cleanup()
