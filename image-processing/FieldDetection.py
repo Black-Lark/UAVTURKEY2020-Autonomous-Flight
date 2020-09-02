04b84ab6261d24d7a8ebca03f2084894181eaf46
sametoguten <samet.oguten@agu.edu.tr>
2020-09-02 20:53:46 +0300

    Update FieldDetection.py

--- image-processing/FieldDetection.py
+++ image-processing/FieldDetection.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(-1)
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
