9e4ba3229e7b3606e6ff9a2f3aeda5bc4f68bbd8
Bilal KABAŞ <bilalkabas@icloud.com>
2020-09-04 16:14:48 +0300

    Update Camera_Recording_Test.py

--- image-processing/Camera_Recording_Test.py
+++ image-processing/Camera_Recording_Test.py
@@ -8,31 +8,16 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('recording_test1.avi',fourcc, 30.0, (640,480))
 while(cap.isOpened()):
     ret, frame = cap.read()
-    frame = cv2.flip(frame,1)
     if ret == True:
-        # Filter red color
-        # frame = cv2.bilateralFilter(frame,9,75,75)
-        frame_hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        mask1 = cv2.inRange(frame_hsv, (0, 70, 50), (10, 255, 255))
-        mask2 = cv2.inRange(frame_hsv, (170, 70, 50), (180, 255, 255))
-        mask = mask1 + mask2
-        # Edge detection
-        canny_output = cv2.Canny(mask, 10, 10, 20)
-        # Find the center
-        pixels = np.where(canny_output == 255)
-        cX = np.average(pixels[1])
-        cY = np.average(pixels[0])
-        
-        # cX cY -> NaN
-        cv2.circle(canny_output, (int(cX),int(cY)), 3, (255,255,255), thickness=10, lineType=8, shift=0)
+
         # Show the frame
         out.write(frame)
-        cv2.imshow("contours", canny_output)
+        
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Video stream has been terminated.')
             break
-
+        cv2.imshow("frame",frame)
     else:
         print('Video stream has been corrupted.')
         break
