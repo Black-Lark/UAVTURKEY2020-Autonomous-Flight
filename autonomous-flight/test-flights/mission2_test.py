a8fb8afd96c2fa7d3e0aff58d9ae4448f833b532
AbdulkadirCeylan <abdulkadir.ceylan@agu.edu.tr>
2020-09-05 23:00:40 +0300

    Update mission2_test.py

--- autonomous-flight/test-flights/mission2_test.py
+++ autonomous-flight/test-flights/mission2_test.py
@@ -1,11 +1,12 @@
 import numpy as np
+import cv2
 #from picamera import PiCamera
 from pymavlink import mavutil
 from dronekit import connect, VehicleMode, LocationGlobalRelative,Vehicle, LocationGlobal, Command
 import time
 
 vehicle = connect("tcp:127.0.0.1:5762", wait_ready=True)
-cap = cv2.VideoCapture('iki-üç.avi')
+cap = cv2.VideoCapture(0)
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
@@ -42,7 +43,8 @@ def arm_and_takeoff(aTargetAltitude):
 
 def first_tour(): 
     cmds = vehicle.commands
-    cmds.clear() 
+    cmds.clear()
+    vehicle.flush 
     cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, 5))
     cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6941650 ,35.4606727 , 5))#1
     cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942204 ,35.4605145 , 5))#2 Su alma alanı
@@ -61,25 +63,91 @@ def first_tour():
     print(" Upload new commands to vehicle")
     cmds.upload()
 
+def second_tour(lat,lon):
+    cmds = vehicle.commands
+    cmds.clear()
+    vehicle.flush() 
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, 5))
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6941650 ,35.4606727 , 5))#1
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942204 ,35.4605145 , 3))#2 Su alma alanı
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942204 ,35.4605145 , 0.25))#2 Su alma alanı
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942204 ,35.4605145 , 5))#2 Su alma alanı
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, lat ,lon , 5))#3 Su bırakma alanı
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, lat ,lon , 0.25))#3 Su bırakma alanı
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942761, 35.4603562 , 5))#3
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942703, 35.4603022 , 5))#4
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942441, 35.4602586 , 5))#5
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942012, 35.4602462 , 5))#6
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6941599, 35.4602650 , 5))#7
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6940453, 35.4605875 , 5))#8
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6940568, 35.4606505 , 5))#9
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6940890, 35.4606901 , 5))#10
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6941342, 35.4606888 , 5))#11
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6941650, 35.4606727 , 5))#12
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6941650, 35.4606727 , 5))#12/
+    
+    cmds.upload()
+
+
 arm_and_takeoff(5)
 first_tour()
 vehicle.mode = VehicleMode("AUTO")
 vehicle.commands.next=0
-
-
-
+# Detect coordinates of the field
+lat = 0
+lon = 0
+frame_pos = []
 while vehicle.commands.next <=12:
     nextwaypoint=vehicle.commands.next
 
     if vehicle.commands.next == 3:
         ret, frame = cap.read()
+
         if ret == True:
             # Filter red color
-            # frame = cv2.bilateralFilter(frame,9,75,75)
+            frame = cv2.bilateralFilter(frame,9,75,75)
             frame_hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
             mask1 = cv2.inRange(frame_hsv, (0, 70, 50), (10, 255, 255))
             mask2 = cv2.inRange(frame_hsv, (170, 70, 50), (180, 255, 255))
             mask = mask1 + mask2
             white_pixels = np.where(mask==255)
-            print(len(white_pixels[0]))
-            if len(white_pixels[0])
+            cX = np.average(white_pixels[1])
+            cY = np.average(white_pixels[0])
+            
+            # Small noise elimination
+            if len(white_pixels[0]) > 5000:
+                # Object location detection
+                if (220 < cY < 350):
+                    img = np.zeros((480,640,1),np.uint8)    
+                    cv2.circle(img, (int(cX),int(cY)), 85, (255,255,255), thickness=-1, lineType=8, shift=0)
+                    intersection = cv2.bitwise_and(img,mask)
+                    intersection_length = np.where(intersection==255)
+
+                    print(len(intersection_length[0]))
+                    # Grande noise elimination
+                    if len(intersection_length[0]) > 5000:
+                        # Show the frame
+
+                        cv2.imshow("mask", mask)
+                        cv2.imshow("black", img)
+                        cv2.imshow("intersection", intersection)
+                        
+                        print("Field Detected")
+                        lat = vehicle.location.global_relative_frame.lat
+                        lon = vehicle.location.global_relative_frame.lon
+                        frame_pos.append(lat,lon)
+            # Show the frame        
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print('Video stream has been terminated.')
+                break
+
+        else:
+            print('Video stream has been corrupted.')
+            break
+second_tour(lat,lon)
+vehicle.commands.next=0
+nextwaypoint=0
+while vehicle.commands.next <=17:
+    nextwaypoint=vehicle.commands.next
+
