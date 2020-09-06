1453124fb50a24cec8da3d8595161cf058306b76
Bilal KABAŞ <bilalkabas@icloud.com>
2020-09-06 16:55:21 +0300

    Update Camera_Recording_Test.py

--- image-processing/Camera_Recording_Test.py
+++ image-processing/Camera_Recording_Test.py
@@ -1,15 +1,18 @@
 import cv2
 import numpy as np
-
+from time import gmtime, strftime
 cap = cv2.VideoCapture(0)
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-print(frame_height, " " ,frame_width)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+file_name = strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + ".avi"
+print(file_name)
+
+out = cv2.VideoWriter(file_name,fourcc, 30, (640,480))
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         # Show the frame
-        # out.write(frame)
+        out.write(frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Video stream has been terminated.')
             break
