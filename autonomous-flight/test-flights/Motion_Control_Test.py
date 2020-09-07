a2a32a650b0caa94462f1168f922f25f97047279
AbdulkadirCeylan <abdulkadir.ceylan@agu.edu.tr>
2020-09-07 19:17:55 +0300

    Update Motion_Control_Test.py

--- autonomous-flight/test-flights/Motion_Control_Test.py
+++ autonomous-flight/test-flights/Motion_Control_Test.py
@@ -43,6 +43,24 @@ def goto(dNorth, dEast, alt, gotoFunction=vehicle.simple_goto):
     #targetDistance = get_distance_metres(currentLocation, targetLocation)
     gotoFunction(targetLocation)
 
+def condition_yaw(heading, relative=False):
+   
+    if relative:
+        is_relative = 1 #yaw relative to direction of travel
+    else:
+        is_relative = 0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
 
 def arm_and_takeoff(aTargetAltitude):
     while not vehicle.is_armable:
@@ -126,6 +144,7 @@ while True:
 
                 if RSquare > 20:
                     goto(north,east,vehicle.location.global_relative_frame.alt) # field = True #Field is centered. Ready to drop the water
+                    condition_yaw(degree)
                 elif vehicle.location.global_relative_frame.alt>= 1 and RSquare < 20:
                     goto(0,0,(vehicle.location.global_relative_frame.alt-0.25))
                     print("landing")     
