6a2c656ad7b517bf2b2131412ac336f7781ce033
AbdulkadirCeylan <abdulkadir.ceylan@agu.edu.tr>
2020-09-07 13:09:21 +0300

    Create dummy_waypoints.py

---
+++ autonomous-flight/test-flights/waypoints/dummy_waypoints.py
@@ -0,0 +1,65 @@
+import numpy as np
+import cv2
+#from picamera import PiCamera
+from pymavlink import mavutil
+from dronekit import connect, VehicleMode, LocationGlobalRelative,Vehicle, LocationGlobal, Command
+import time
+from time import gmtime, strftime
+
+# vehicle = connect("/dev/serial0", wait_ready=True, baud=921000)
+vehicle = connect("tcp:127.0.0.1:5762", wait_ready=True)
+
+def arm_and_takeoff(aTargetAltitude):
+    print("Basic pre-arm checks")
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    time.sleep(2)
+
+    print(vehicle.mode)
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.90:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+def first_tour(): 
+    cmds = vehicle.commands
+    cmds.clear()
+    vehicle.flush()
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6940306 ,35.4605144 , 5))#2
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6942243 ,35.4604755 , 5))#3
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6941650 ,35.4606727 , 5))#4
+    cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 38.6941650 ,35.4606727 , 5))#5
+    
+    print(" Upload new commands to vehicle")
+    cmds.upload()
+
+
+arm_and_takeoff(5)
+first_tour()
+vehicle.mode = VehicleMode("AUTO")
+vehicle.commands.next=0
+
+while vehicle.commands.next <=2:
+    nextwaypoint=vehicle.commands.next
+print("Out")
+vehicle.mode= VehicleMode("LAND")
