00879474e1292ed76a01ea3a0eef548d40be6fbd
AbdulkadirCeylan <abdulkadir.ceylan@agu.edu.tr>
2020-09-08 10:26:44 +0300

    Update Motion_Control_Test.py

--- autonomous-flight/test-flights/Motion_Control_Test.py
+++ autonomous-flight/test-flights/Motion_Control_Test.py
@@ -6,8 +6,8 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative,Vehicle, Locat
 import time
 import math
 
-vehicle = connect("tcp:127.0.0.1:5762", wait_ready=True)
-#vehicle = connect("/dev/serial0", wait_ready=True, baud=921000)
+#vehicle = connect("tcp:127.0.0.1:5762", wait_ready=True)
+vehicle = connect("/dev/serial0", wait_ready=True, baud=921000)
 
 def get_location_metres(original_location, dNorth, dEast):
     
