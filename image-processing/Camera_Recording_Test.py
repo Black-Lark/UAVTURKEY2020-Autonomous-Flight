2ccf834f2c241b190f6813271dc20eb7d454938b
AbdulkadirCeylan <abdulkadir.ceylan@agu.edu.tr>
2020-09-08 13:16:39 +0300

    Update Camera_Recording_Test.py

--- image-processing/Camera_Recording_Test.py
+++ image-processing/Camera_Recording_Test.py
@@ -12,8 +12,12 @@ out = cv2.VideoWriter(file_name,fourcc, 30, (640,480))
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
+        frame_hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        mask1 = cv2.inRange(frame_hsv, (0, 70, 50), (10, 255, 255))
+        mask2 = cv2.inRange(frame_hsv, (165, 70, 50), (180, 255, 255))
+        mask = mask1 + mask2
         # Show the frame
-        out.write(frame)
+        out.write(mask)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Video stream has been terminated.')
             break
