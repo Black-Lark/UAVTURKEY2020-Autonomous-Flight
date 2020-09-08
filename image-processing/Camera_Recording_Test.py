b3de0a6e03622761fbbd84c3e17a3880ae78fe04
sametoguten <samet.oguten@agu.edu.tr>
2020-09-08 14:46:02 +0300

    Update Camera_Recording_Test.py

--- image-processing/Camera_Recording_Test.py
+++ image-processing/Camera_Recording_Test.py
@@ -4,7 +4,7 @@ from time import gmtime, strftime
 
 cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-file_name = strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + ".h264"
+file_name = strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + ".avi"
 out = cv2.VideoWriter(file_name,fourcc, 25, (640,480))
 print(file_name)
 
