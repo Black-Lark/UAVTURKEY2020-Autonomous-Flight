6875e9580c7d0714740bad0d47b42c085092a710
AbdulkadirCeylan <abdulkadir.ceylan@agu.edu.tr>
2020-09-08 13:25:52 +0300

    Update Camera_Recording_Test.py

--- image-processing/Camera_Recording_Test.py
+++ image-processing/Camera_Recording_Test.py
@@ -1,14 +1,18 @@
 import cv2
 import numpy as np
 from time import gmtime, strftime
-
+from picamera import PiCamera
+from time import sleep
+dronecamera = PiCamera()
+dronecamera.start_recording('C:\Users\thosl\Documents\GitHub/video_test.h264')
 cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-file_name = strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + ".avi"
-out = cv2.VideoWriter(file_name,fourcc, 25, (640,480))
-print(file_name)
-
-out = cv2.VideoWriter(file_name,fourcc, 30, (640,480))
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#file_name = strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + ".avi"
+#out = cv2.VideoWriter(file_name,fourcc, 25, (640,480))
+#print(file_name)
+sleep(10)
+dronecamera.stop_recording()
+#out = cv2.VideoWriter(file_name,fourcc, 30, (640,480))
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
@@ -17,7 +21,7 @@ while(cap.isOpened()):
         mask2 = cv2.inRange(frame_hsv, (165, 70, 50), (180, 255, 255))
         mask = mask1 + mask2
         # Show the frame
-        out.write(mask)
+        #out.write(mask)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Video stream has been terminated.')
             break
