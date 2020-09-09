c4decde6db75569e06d3829c7fbd0cb7e6603a8c
Bilal KABAŞ <bilalkabas@icloud.com>
2020-09-09 19:06:41 +0300

    Update live-video.py

--- modelling/live-video.py
+++ modelling/live-video.py
@@ -8,12 +8,14 @@ while(cap.isOpened()):
     if ret == True:
         # Show the frame
         cv2.imshow("frame", frame)
+        cv2.imwrite("224.png", frame)
+        break
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Video stream has been terminated.')
             break
     else:
         print('Video stream has been corrupted.')
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
