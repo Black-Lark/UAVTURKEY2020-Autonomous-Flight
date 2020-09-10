f104fbf4e694803e1c606acf975c4f7201bb004b
Bilal KABAŞ <bilalkabas@icloud.com>
2020-09-10 14:36:33 +0300

    Update Motion_Control_Test.py

--- autonomous-flight/test-flights/Motion_Control_Test.py
+++ autonomous-flight/test-flights/Motion_Control_Test.py
@@ -32,11 +32,46 @@ def arm_and_takeoff(aTargetAltitude):
             break
         time.sleep(1)
 
+def get_location_metres(original_location, dNorth, dEast):
+    
+    earth_radius = 6378137.0 #Radius of "spherical" earth
+    #Coordinate offsets in radians
+    dLat = dNorth/earth_radius
+    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+
+    #New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180/math.pi)
+    newlon = original_location.lon + (dLon * 180/math.pi)
+
+    if type(original_location) is LocationGlobal:
+        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
+    elif type(original_location) is LocationGlobalRelative:
+        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
+    else:
+        raise Exception("Invalid Location object passed")
+        
+    return targetlocation
+
+def get_distance_metres(aLocation1, aLocation2):
+
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+def goto(dNorth, dEast,alt, gotoFunction=vehicle.simple_goto):
+    
+    currentLocation = vehicle.location.global_relative_frame
+    currentLocation.alt = alt
+    targetLocation = get_location_metres(currentLocation, dNorth, dEast)
+    targetDistance = get_distance_metres(currentLocation, targetLocation)
+    gotoFunction(targetLocation)
+
+
 arm_and_takeoff(3)
 
 while True:
     if vehicle.location.global_relative_frame.alt>0.5:
-        vehicle.mode= VehicleMode("LAND")
+        goto(0,0,0.5)
     else:
-        vehicle.mode = VehicleMode("GUIDED")
+        goto(0,0,vehicle.location.global_relative_frame.alt)
 
