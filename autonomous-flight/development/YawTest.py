0a269f8b5ac42ea0d8aaf6dc525f55e1cb4fe099
Bilal KABAŞ <bilalkabas@icloud.com>
2020-09-11 13:02:09 +0300

    Create YawTest.py

---
+++ autonomous-flight/development/YawTest.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import numpy as np
+import cv2
+from pymavlink import mavutil
+from dronekit import connect, VehicleMode, LocationGlobalRelative,Vehicle, LocationGlobal, Command
+from time import gmtime, strftime 
+import time
+import math
+
+vehicle = connect("/dev/serial0", wait_ready=True, baud=921000)
+
+while True:
+    print(vehicle.attitude.yaw)
+    time.sleep(0.5)
