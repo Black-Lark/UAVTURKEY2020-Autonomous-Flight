92e84c16ff511fe33d044408e62ea04b742ef9df
sametoguten <samet.oguten@agu.edu.tr>
2020-09-12 22:53:47 +0300

    Update FS90rTest.py

--- raspberrypi/FS90rTest.py
+++ raspberrypi/FS90rTest.py
@@ -1,5 +1,8 @@
+from gpiozero import LED, Button
+from signal import pause 
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(32, GPIO.OUT)
@@ -10,6 +13,6 @@ t0 = time.time()
 
 while time.time() - t0 < 2:
     pwm.ChangeDutyCycle(10) # for cw use (5)  
-      
+
 pwm.stop()
 GPIO.cleanup()
