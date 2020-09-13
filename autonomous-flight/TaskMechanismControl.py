7a2d93d8479052445532afbdb1bb1b3e6344eda3
sametoguten <samet.oguten@agu.edu.tr>
2020-09-13 11:45:18 +0300

    Update TaskMechanismControl.py

--- autonomous-flight/TaskMechanismControl.py
+++ autonomous-flight/TaskMechanismControl.py
@@ -13,8 +13,8 @@ pump_motor_relay = LED(7)
 dc_motor_relay = LED(8)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(33, GPIO.OUT)
-pwm=GPIO.PWM(33, 50)
+GPIO.setup(32, GPIO.OUT)
+pwm=GPIO.PWM(32, 50)
 
 while level2 and level3 is False:
     if level0 is True:
