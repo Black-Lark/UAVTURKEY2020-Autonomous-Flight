1cd7d157f57c87adead76a52089181dd94109024
sametoguten <samet.oguten@agu.edu.tr>
2020-09-13 11:14:36 +0300

    Update TaskMechanismControl.py

--- autonomous-flight/TaskMechanismControl.py
+++ autonomous-flight/TaskMechanismControl.py
@@ -1,18 +1,45 @@
 from gpiozero import LED, Button
 from signal import pause 
+import RPi.GPIO as GPIO
 import time
 
-level0 = Button(6)
-level1 = Button(13)
-level2 = Button(19)
-level3 = Button(26)
+# Water sensor
+level0 = Button(6) # Pompa motors
+level1 = Button(13) # Kabın içerisinde zeminde 
+level2 = Button(19) # kabın içerisinde yukarıda 
+level3 = Button(26) # hava valfinin orada
 
+pump_motor_relay = LED(7)
 dc_motor_relay = LED(8)
-dc_motor_relay.on()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(33, GPIO.OUT)
+pwm=GPIO.PWM(33, 50)
 
-time.sleep(2)
+while level2 and level3 is False:
+    if level0 is True:
+        pump_motor_relay.off()
 
-dc_motor_relay.off()
+pump_motor_relay.on()
+pwm.start(0)
+t0 = time.clock()
+t0 = time.time()
+
+while time.time() - t0 < 2:
+    pwm.ChangeDutyCycle(10) # for cw use (5)    
+
+pwm.stop()
+GPIO.cleanup()
+
+
+
+
+#dc_motor_relay = LED(8)
+#dc_motor_relay.on()
+
+#time.sleep(2)
+
+#dc_motor_relay.off()
 
 
 # while True:
@@ -28,4 +55,4 @@ dc_motor_relay.off()
 
 # dcmotor.on()
 
-pause()
+#pause()
