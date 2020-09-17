5fee3d7e1076c9a9ffdb8cae678141e0e3ae3c21
sametoguten <samet.oguten@agu.edu.tr>
2020-09-17 21:25:45 +0300

    Update Camera_Recording_Test.py

--- image-processing/Camera_Recording_Test.py
+++ image-processing/Camera_Recording_Test.py
@@ -7,7 +7,7 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 file_name = strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + ".avi"
 out = cv2.VideoWriter(file_name,fourcc, 25, (640,480))
 print(file_name)
-
+counter = 0
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
@@ -16,6 +16,7 @@ while(cap.isOpened()):
         # mask2 = cv2.inRange(frame_hsv, (165, 70, 50), (180, 255, 255))
         # mask = mask1 + mask2
         # Show the frame
+        counter = counter + 1
         out.write(frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Video stream has been terminated.')
