62acd1352a06cea05ee23f06a905ae3e5f9de297
Bilal KABAŞ <bilalkabas@icloud.com>
2021-02-27 02:07:08 +0300

    Files organized

--- image-processing/modelling/modelling.m
+++ image-processing/modelling/modelling.m
@@ -1,17 +1,18 @@
-pixel_to_cm = 120./(640.*yuzde120/100);
+% percent_120: percent values for 120 cm length
+pixel_to_cm = 120./(640.*percent_120/100);
+% percent_211: percent values for 211 cm length
+pixel_to_cm_2 = 211./(640.*percent_211/100);
 
-pixel_to_cm2 = 211./(640.*yuzde211/100);
-
-plot(mesafe, pixel_to_cm)
+% distance_120: distance values for 120 cm length
+plot(distance_120, pixel_to_cm)
 axis square
-
 hold on
-plot(mesafe2, pixel_to_cm2)
 
-p = polyfit(mesafe, pixel_to_cm, 1)
-xi = min(mesafe):0.01:800;
+% distance_211: distance values for 211 cm length
+plot(distance_211, pixel_to_cm_2)
+
+% Linear regression
+p = polyfit(distance, pixel_to_cm, 1)
+xi = min(distance):0.01:800;
 yi = polyval(p, xi);
-
 hold on
-
-plot(xi, yi)
